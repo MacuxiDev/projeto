ed91b0aa06f33c1e14817de2e319240f28751c5b
macuxiDev <guilhermes301@gmail.com>
2026-06-14 18:38:07 -0300

    fix: adjust route mapping for war context

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"", "/"})
     public String home() {
         // Altere o texto destas tags h1 e h2 para criar as Versões 1.0, 2.0 e 3.0
         return "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>"
