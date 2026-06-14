c6573ba4d0967156f9d9b8232f34def32b3780b0
macuxiDev <guilhermes301@gmail.com>
2026-06-14 18:47:12 -0300

    fix: mapeamento de rota universal para war

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping({"", "/"})
+    // O "/**" garante que o Spring Boot vai capturar a requisição na raiz da subpasta do Tomcat
+    @GetMapping("/**")
     public String home() {
-        // Altere o texto destas tags h1 e h2 para criar as Versões 1.0, 2.0 e 3.0
         return "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>"
              + "<h1>Ambiente de Staging (Spring Boot + Java 21)</h1>"
              + "<h2 style='color:#27ae60;'>Versão 1.0 - Primeiro Deploy com Sucesso!</h2>"
